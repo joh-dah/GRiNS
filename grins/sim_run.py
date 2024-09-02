39ae10ab62e5cf8d9bac7176914b59500911b1b8
Harshavardhan-BV <bharshavardh@iisc.ac.in>
2024-09-02 22:13:20 +0530

    Timeseries simulations

--- grins/sim_run.py
+++ grins/sim_run.py
@@ -10,6 +10,7 @@ from diffrax import diffeqsolve, ODETerm, SaveAt, Tsit5, PIDController, Event, s
 import jax.numpy as jnp
 from jax import vmap, pmap
 import pandas as pd
+import xarray as xr
 
 def ODE_topo(topo_name, simdir):
     """
@@ -39,7 +40,7 @@ def solve_ode(term, solver, t0, t1, dt0, y0, args):
         t1 (float): The final time.
         dt0 (float): The initial time step.
         y0 (array-like): The initial state of the system.
-        args (tuple): Additional arguments to be passed to the ODE term.
+        args (array-like): Additional arguments to be passed to the ODE term.
 
     Returns:
         sol.ys (jnp.array): The final state of the system at time t1.
@@ -84,3 +85,58 @@ def get_steady_states(init_cond, param_vals, term):
     # Convert to dataframe
     sol_li = pd.DataFrame(sol_li, columns=node_li, index = pd.MultiIndex.from_product([param_vals.index, init_cond.index], names=["ParaNum", "InitCondNum"]))
     return sol_li
+#%%
+def solve_ode_timeseries(term, solver, y0, args, t):
+    """
+    Solve the ordinary differential equations (ODEs) and return the time series data.
+
+    Parameters:
+        term (callable): The term representing the ODE to be solved.
+        solver (object): The solver to be used for solving the ODE.
+        y0 (array-like): The initial state of the system.
+        args (array-like): Additional arguments to be passed to the ODE term.
+        t (array-like): The time points at which to evaluate the solution.
+
+    Returns:
+        sol.ys (jnp.array): The time series data of the system.
+    """
+    # Add line to convert y0 to a tuple
+    y0 = tuple(y0)
+    # Diffrax solve function
+    sol = diffeqsolve(term, solver, t[0], t[-1], (t[1]-t[0]), y0, args, saveat=SaveAt(ts=t), stepsize_controller=PIDController(rtol=1e-5, atol=1e-6), max_steps=None)
+    # Return the solution
+    return jnp.array(sol.ys).T
+#%%
+def ode_timeseries_param(init_cond, param_vals, t, term):
+    """
+    Solve a system of ordinary differential equations (ODEs) over a given time range for multiple combinations of initial conditions and parameter values.
+
+    Parameters:
+        init_cond (DataFrame): DataFrame containing the initial conditions for each node.
+        param_vals (DataFrame): DataFrame containing the parameter values for each combination.
+        t (array-like): Array-like object representing the time range.
+        term (callable): Callable representing the ODE system.
+
+    Returns:
+        sol_li (DataArray): xarray DataArray containing the solutions of the ODEs for each combination of initial conditions and parameter values.
+    """
+    # Get the node list
+    node_li = list(init_cond.columns)
+    # Merge the initial conditions and parameter values dataframes with combinations of inital conditions and parameter values
+    comb_arr = jnp.array(param_vals.merge(init_cond, how="cross"))
+    # Run the solve_ode function over the combinations array
+    start = time.time()
+    sol_li = vmap(
+        lambda i: solve_ode_timeseries(
+            term,
+            Tsit5(),
+            i[-len(node_li):], #Indexing changed
+            i[:-len(node_li)],
+            t
+        ),
+    )(comb_arr)
+    print(f"Time taken to solve the ODEs: {time.time() - start}")
+    # Convert to xarray dataarray
+    sol_li = xr.DataArray(sol_li.transpose(1, 2, 0), coords=[t,node_li, pd.MultiIndex.from_product([param_vals.index, init_cond.index])], dims=[ "Time","Node", "Combination"])
+    return sol_li
+# %%
