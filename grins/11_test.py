56815ddeb1154de681c0480f0882751e3d13481a
MoltenEcdysone09 <pradyumnah@iisc.ac.in>
2024-10-14 16:59:54 +0530

    Added steady state column to solution df.

--- grins/11_test.py
+++ grins/11_test.py
@@ -10,7 +10,7 @@ from diffrax import (
 
 import jax.numpy as jnp
 import numpy as np
-from jax import lax, vmap, jit, clear_caches
+from jax import lax, clear_caches
 import pandas as pd
 import glob
 import os
@@ -121,9 +121,10 @@ def parameterise_solveode(
             event=Event(steady_state_event()),
         )
         return [s[-1] for s in sol.ys] + [
-            inicond[pi_row[0]][-1],
-            paramvals[pi_row[1]][-1],
-            sol.ts[-1],
+            inicond[pi_row[0]][-1],  # The initial condition number
+            paramvals[pi_row[1]][-1],  # The parameter number
+            sol.ts[-1],  # The end time
+            jnp.astype(sol.event_mask, jnp.int32),  # The steady state event mask
         ]
 
     return solve_ode, icprm_comb
@@ -131,36 +132,49 @@ def parameterise_solveode(
 
 # Function to solve for a given replicate folder
 def solve_replicate(
+    ode_sys,
     repfl,
     topo_name,
+    init_cond,
+    param_vals,
     batch_size=100000,
     num_chunks=5,
-    param_li=None,
-    init_li=None,
     round=2,
     parequet=True,
     compress=None,
+    solver=Tsit5(),
+    t0=0,
+    t1=200,
+    dt0=0.1,
+    ts=None,
+    retol=1e-5,
+    atol=1e-6,
+    max_steps=None,
 ):
-    print("Replicate Number: ", repfl[:-1])
+    print("Replicate Folder: ", repfl[:-1])
     # Get the initial conditions csv file
-    init_cond = pd.read_csv(f"{repfl}{topo_name}_init_conds_{repfl[:-1]}.csv")
+    # init_cond = pd.read_csv(f"{repfl}{topo_name}_init_conds_{repfl[:-1]}.csv")
     # Number of nodes
     num_nodes = len(init_cond.columns) - 1
     print(f"Number of nodes: {num_nodes}")
     # Get the parameter values csv file
-    param_vals = pd.read_csv(f"{repfl}{topo_name}_params_{repfl[:-1]}.csv")
-    # If the parameter list is provided, then subset the parameter values
-    if param_li is not None:
-        param_vals = param_vals[param_vals["ParaNum"].isin(param_li)]
-    # If the initial condition list is provided, then subset the initial conditions
-    if init_li is not None:
-        init_cond = init_cond[init_cond["InitCondNum"].isin(init_li)]
+    # param_vals = pd.read_csv(f"{repfl}{topo_name}_params_{repfl[:-1]}.csv")
     #  #Time the solving of the ODEs
     start = time.time()
     # Getting the solve ode function and the index combinations of paramters and initial conditions
     # Not Scan
     solve_ode_fn, icprm_comb = parameterise_solveode(
-        odesys_11, inicond=init_cond, paramvals=param_vals
+        ode_sys,
+        inicond=init_cond,
+        paramvals=param_vals,
+        solver=solver,
+        t0=t0,
+        t1=t1,
+        dt0=dt0,
+        ts=ts,
+        retol=retol,
+        atol=atol,
+        max_steps=max_steps,
     )
     # Subset icprm_comb
     print(f"Number of actual combinations: {len(icprm_comb)}")
@@ -168,8 +182,12 @@ def solve_replicate(
     # Getting chunks of the icprm_comb
     icprm_chunks = jnp.array_split(icprm_comb, num_chunks)
     # Stacking into a 3D array
+    # Number of chunks should give same shaped arrays - will give error otherwise
+    # Can be fixed if its used as a list instead of a jax 3d-array
     icprm_chunks = jnp.stack(icprm_chunks)
-    print(icprm_chunks.shape)
+    print(
+        f"No. of chunks: {icprm_chunks.shape[0]}\nNo. of combinations per chunk: {icprm_chunks.shape[1]}"
+    )
     # Saving the solutions
     sol_li = []
     # Runnning the solve_ode_fn by mapping over the icprm_chunks in for loop
@@ -181,30 +199,33 @@ def solve_replicate(
         )
         # Converting the output to a numpy array and appending to the solution list
         sol_li.append(np.array(batch_sol_li))
-        print(f"Time taken: {time.time() - b_start}")
+        print(f"Time taken for chunk {i}: {time.time() - b_start}")
         # Clear the cache
         clear_caches()
     # Print the time taken to solve the ODEs
-    print(f"Time taken to solve the ODEs: {time.time() - start}")
+    print(f"Total time taken: {time.time() - start}")
     # Concatenate the solution list along the rows
     sol_li = np.concatenate(sol_li, axis=1).T
     # Get the columns names of the initial conditions -> gives node names
     node_li = [i for i in init_cond.columns if "InitCondNum" not in i]
     # Convert the solution list to a data frame
+    sol_li = pd.DataFrame(
+        np.round(sol_li, round),
+        columns=node_li + ["InitCondNum", "ParaNum", "TimeEnd", "SteadyState"],
+    )
     # Save the solution dataframe to the relevent replicate folder
     if parequet:
-        pd.DataFrame(
-            np.round(sol_li, round),
-            columns=node_li + ["InitCondNum", "ParaNum", "TimeEnd"],
-        ).to_parquet(
+        print(f"Saving solution to parquet ({compress}) at {repfl}.\n")
+        sol_li.to_parquet(
             f"{repfl}{topo_name}_sol_{repfl[:-1]}.parquet.gzip",
             compression=compress,
             index=False,
         )
     else:
+        print(f"Saving solution to csv at {repfl}.\n")
         pd.DataFrame(
             np.round(sol_li, round),
-            columns=node_li + ["InitCondNum", "ParaNum", "TimeEnd"],
+            columns=node_li + ["InitCondNum", "ParaNum", "TimeEnd", "SteadyState"],
         ).to_csv(f"{repfl}{topo_name}_sol_{repfl[:-1]}.csv", index=False)
     # Return True on success
     return True
@@ -227,5 +248,23 @@ print(topo_name)
 
 # Loop through each of the replicate folders
 for repfl in rep_folders:
+    # # Get the initial conditions csv file
+    init_cond = pd.read_csv(f"{repfl}{topo_name}_init_conds_{repfl[:-1]}.csv")
+    # # Get the parameter values csv file
+    param_vals = pd.read_csv(f"{repfl}{topo_name}_params_{repfl[:-1]}.csv")
+    # # Subset 10 rows of the initial conditions and parameter values
+    # init_cond = init_cond[:10]
+    # param_vals = param_vals[:10]
     # Solve the ODEs for the replicate folder
-    solve_replicate(repfl, topo_name, round=4, parequet=True, compress="gzip")
+    solve_replicate(
+        odesys_11,
+        repfl,
+        topo_name,
+        init_cond=init_cond,
+        param_vals=param_vals,
+        round=4,
+        num_chunks=4,
+        parequet=True,
+        compress="gzip",
+        solver=Tsit5(),
+    )
