efb8ebe06a6d25a82740280437599e52f74d12b5
MoltenEcdysone09 <pradyumnah@iisc.ac.in>
2024-10-19 17:15:10 +0530

    Added time series support and updated solution df creation functions.

--- grins/11_test.py
+++ grins/11_test.py
@@ -15,7 +15,7 @@ import pandas as pd
 import glob
 import os
 import time
-import subprocess
+from multiprocessing import Pool
 
 
 # Positive Shifted Hill function
@@ -77,6 +77,31 @@ def _gen_combinations(num_init_conds, num_params):
     return icprm_comb
 
 
+# Function which takes in the ode solution and then returns the formatted version of the solution
+def _format_sol(sol, init_cond_num, param_num):
+    sol_vals = sol.ys
+    # Convert sol_vals into a 2d array
+    sol_vals = jnp.array(sol_vals).T
+    # Get the time point values
+    time_points = sol.ts
+    # Add the time points to the sol_vals as a column
+    sol_vals = jnp.hstack((sol_vals, jnp.array(time_points).reshape(-1, 1)))
+    # Return the solution list of list, the initial condition number, the parameter nuumber and steady state event mask
+    if len(sol_vals) == 1:
+        return [
+            sol_vals,  # An array of the steady state values
+            init_cond_num,  # The initial condition number
+            param_num,  # The parameter number
+            jnp.astype(sol.event_mask, jnp.int32),  # The steady state event mask
+        ]
+    else:
+        return (
+            sol_vals,  # An array of the time series values
+            init_cond_num,
+            param_num,  # The initial condition number
+        )
+
+
 # A closure to parameterise all the non initial and paramter values in the ODE system
 def parameterise_solveode(
     odesys,
@@ -95,9 +120,12 @@ def parameterise_solveode(
     if ts is None:
         saveat = SaveAt(t1=True)
     else:
-        t0 = ts[0]
-        t1 = ts[-1]
-        saveat = SaveAt(t1=True, ts=ts)
+        # Make sure the time steps are sorted
+        ts = sorted(ts)
+        if t1 < ts[-1]:
+            t1 = ts[-1]
+        # Make saaveat to be steps
+        saveat = SaveAt(ts=ts)
     stepsize_controller = PIDController(rtol=retol, atol=atol)
     # Convert the initial conditions and parameter values to a jax array
     inicond = jnp.array(inicond, dtype=jnp.float32)
@@ -105,29 +133,74 @@ def parameterise_solveode(
     # Generate the combinations of the indices of the initial conditions and parameter values
     icprm_comb = _gen_combinations(len(inicond), len(paramvals))
 
-    # Function to solve the ODEs
-    def solve_ode(pi_row):
-        sol = diffeqsolve(
-            ode_term,  # ODETerm
-            solver,  # Solver
-            t0,  # Start time
-            t1,  # End time
-            dt0,  # Time step
-            tuple(inicond[pi_row[0]][:-1]),  # Initial conditions
-            tuple(paramvals[pi_row[1]][:-1]),  # Parameters
-            saveat=saveat,
-            stepsize_controller=stepsize_controller,
-            max_steps=max_steps,
-            event=Event(steady_state_event()),
-        )
-        return [s[-1] for s in sol.ys] + [
-            inicond[pi_row[0]][-1],  # The initial condition number
-            paramvals[pi_row[1]][-1],  # The parameter number
-            sol.ts[-1],  # The end time
-            jnp.astype(sol.event_mask, jnp.int32),  # The steady state event mask
-        ]
+    # Check if number of time steps to save is None
+    if ts is None:
+        # Function to solve the ODEs
+        def solve_ode(pi_row):
+            sol = diffeqsolve(
+                ode_term,  # ODETerm
+                solver,  # Solver
+                t0,  # Start time
+                t1,  # End time
+                dt0,  # Time step
+                tuple(inicond[pi_row[0]][:-1]),  # Initial conditions
+                tuple(paramvals[pi_row[1]][:-1]),  # Parameters
+                saveat=saveat,
+                stepsize_controller=stepsize_controller,
+                max_steps=max_steps,
+                event=Event(steady_state_event()),
+            )
+            return _format_sol(sol, inicond[pi_row[0]][-1], paramvals[pi_row[1]][-1])
+    else:
+        # Function to solve the ODEs
+        def solve_ode(pi_row):
+            sol = diffeqsolve(
+                ode_term,  # ODETerm
+                solver,  # Solver
+                t0,  # Start time
+                t1,  # End time
+                dt0,  # Time step
+                tuple(inicond[pi_row[0]][:-1]),  # Initial conditions
+                tuple(paramvals[pi_row[1]][:-1]),  # Parameters
+                saveat=saveat,
+                stepsize_controller=stepsize_controller,
+                max_steps=max_steps,
+                # event=Event(steady_state_event()), # Removed as suing this will give inf values for time points after steady state is reached
+            )
+            return _format_sol(sol, inicond[pi_row[0]][-1], paramvals[pi_row[1]][-1])
 
     return solve_ode, icprm_comb
+
+
+# Function to format the solution list into a dataframe
+def _format_solchunk(sol_li, node_li):
+    # Get the length of the node values list to get the number of time points
+    num_time_points = len(sol_li[0][0])
+    # If the number of time points is 1, then it is a steady state simulation
+    if num_time_points == 1:
+        # Creating the dataframe with the node steady state values
+        sol_df = pd.DataFrame(np.concatenate(sol_li[0]), columns=node_li + ["TimeEnd"])
+        # Add the initial condition number and parameter number columns
+        sol_df["InitCondNum"] = sol_li[1]
+        sol_df["ParaNum"] = sol_li[2]
+        sol_df["SteadyState"] = sol_li[3]
+    else:
+        # Creating the initial condition list
+        init_cond_li = np.concatenate(
+            [[i] * num_time_points for i in sol_li[1].tolist()]
+        )
+        # Creating the parameter number list
+        param_num_li = np.concatenate(
+            [[i] * num_time_points for i in sol_li[2].tolist()]
+        )
+        # Concatenating the node values list and converting to a dataframe
+        sol_df = pd.DataFrame(np.concatenate(sol_li[0]), columns=node_li + ["Time"])
+        # Adding the initial condition number and parameter number columns
+        sol_df["InitCondNum"] = init_cond_li
+        sol_df["ParaNum"] = param_num_li
+        # Convert the dataframe into a multi-index dataframe
+        sol_df = sol_df.set_index(["ParaNum", "InitCondNum", "Time"])
+    return sol_df
 
 
 # Function to solve for a given replicate folder
@@ -176,9 +249,12 @@ def solve_replicate(
         atol=atol,
         max_steps=max_steps,
     )
-    # Subset icprm_comb
     print(f"Number of actual combinations: {len(icprm_comb)}")
-    # icprm_comb = icprm_comb[:100000000]
+    # If number of chunks is greater than the number of combinations, terminate with an error
+    if num_chunks > len(icprm_comb):
+        raise ValueError(
+            "Number of chunks is greater than the number of combinations. Reduce the number of chunks"
+        )
     # Getting chunks of the icprm_comb
     icprm_chunks = jnp.array_split(icprm_comb, num_chunks)
     # Stacking into a 3D array
@@ -188,45 +264,43 @@ def solve_replicate(
     print(
         f"No. of chunks: {icprm_chunks.shape[0]}\nNo. of combinations per chunk: {icprm_chunks.shape[1]}"
     )
+    # Get the columns names of the initial conditions -> gives node names
+    node_li = [i for i in init_cond.columns if "InitCondNum" not in i]
     # Saving the solutions
     sol_li = []
     # Runnning the solve_ode_fn by mapping over the icprm_chunks in for loop
     for i in range(len(icprm_chunks)):
         # Start time
         b_start = time.time()
-        batch_sol_li = lax.map(
+        chunk_sol_li = lax.map(
             solve_ode_fn, icprm_chunks[i], batch_size=int(batch_size) - 1
         )
-        # Converting the output to a numpy array and appending to the solution list
-        sol_li.append(np.array(batch_sol_li))
+        # Formatting the solution list
+        chunk_sol_li = _format_solchunk(chunk_sol_li, node_li)
+        # Appending the output to the solution list
+        sol_li.append(chunk_sol_li)
         print(f"Time taken for chunk {i}: {time.time() - b_start}")
         # Clear the cache
         clear_caches()
     # Print the time taken to solve the ODEs
     print(f"Total time taken: {time.time() - start}")
-    # Concatenate the solution list along the rows
-    sol_li = np.concatenate(sol_li, axis=1).T
-    # Get the columns names of the initial conditions -> gives node names
-    node_li = [i for i in init_cond.columns if "InitCondNum" not in i]
-    # Convert the solution list to a data frame
-    sol_li = pd.DataFrame(
-        np.round(sol_li, round),
-        columns=node_li + ["InitCondNum", "ParaNum", "TimeEnd", "SteadyState"],
-    )
-    # Save the solution dataframe to the relevent replicate folder
-    if parequet:
-        print(f"Saving solution to parquet ({compress}) at {repfl}.\n")
-        sol_li.to_parquet(
-            f"{repfl}{topo_name}_sol_{repfl[:-1]}.parquet.gzip",
-            compression=compress,
-            index=False,
-        )
-    else:
-        print(f"Saving solution to csv at {repfl}.\n")
-        pd.DataFrame(
-            np.round(sol_li, round),
-            columns=node_li + ["InitCondNum", "ParaNum", "TimeEnd", "SteadyState"],
-        ).to_csv(f"{repfl}{topo_name}_sol_{repfl[:-1]}.csv", index=False)
+    # Concatenate the list of dataframes into a single dataframe
+    sol_li = pd.concat(sol_li)
+    print(sol_li)
+    # # Save the solution dataframe to the relevent replicate folder
+    # if parequet:
+    #     print(f"Saving solution to parquet ({compress}) at {repfl}.\n")
+    #     sol_li.to_parquet(
+    #         f"{repfl}{topo_name}_sol_{repfl[:-1]}.parquet.gzip",
+    #         compression=compress,
+    #         index=False,
+    #     )
+    # else:
+    #     print(f"Saving solution to csv at {repfl}.\n")
+    #     pd.DataFrame(
+    #         np.round(sol_li, round),
+    #         columns=node_li + ["InitCondNum", "ParaNum", "TimeEnd", "SteadyState"],
+    #     ).to_csv(f"{repfl}{topo_name}_sol_{repfl[:-1]}.csv", index=False)
     # Return True on success
     return True
 
@@ -252,9 +326,9 @@ for repfl in rep_folders:
     init_cond = pd.read_csv(f"{repfl}{topo_name}_init_conds_{repfl[:-1]}.csv")
     # # Get the parameter values csv file
     param_vals = pd.read_csv(f"{repfl}{topo_name}_params_{repfl[:-1]}.csv")
-    # # Subset 10 rows of the initial conditions and parameter values
-    # init_cond = init_cond[:10]
-    # param_vals = param_vals[:10]
+    # Subset 10 rows of the initial conditions and parameter values
+    init_cond = init_cond[:1000]
+    param_vals = param_vals[:1000]
     # Solve the ODEs for the replicate folder
     solve_replicate(
         odesys_11,
@@ -263,8 +337,9 @@ for repfl in rep_folders:
         init_cond=init_cond,
         param_vals=param_vals,
         round=4,
-        num_chunks=4,
-        parequet=True,
-        compress="gzip",
+        num_chunks=1,
+        parequet=False,
+        # compress="gzip",
         solver=Tsit5(),
+        ts=[1.0, 2.0, 5.0, 60.0, 150.0, 250.0],
     )
